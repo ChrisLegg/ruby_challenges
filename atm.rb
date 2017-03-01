d399cc22d7fc143ea6129117ac6693574811b4fb
Chris Legg <chris@chrislegg.net>
2017-03-01 09:38:58 +0000

    Solve ATM challenge

--- atm.rb
+++ atm.rb
@@ -1,5 +1,13 @@
 require 'rspec'
 
+def atm total, denominations
+  withdraw = denominations.inject({}) do |hash, denomination|
+    hash[denomination] = total.divmod(denomination).first
+    total = total.divmod(denomination).last
+    hash
+  end
+end
+
 describe 'ATM' do
   it 'disepenses the correct denominations to users' do
     expect(atm 110, [100,20,10,5]).to eq({100=>1, 20=>0, 10=>1, 5=>0})
